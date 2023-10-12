c324764f10859eb48757a0b07979a475d0a95f16
박윤환 <dope2514@gmail.com>
2023-10-12 14:28:40 +0900

    Fix public_ip output error

--- outputs.tf
+++ outputs.tf
@@ -5,5 +5,5 @@ output "server_id" {
 
 output "public_ip" {
   description = "서버 Public IP 주소"
-  value       = ncloud_public_ip.this.public_ip
+  value       = length(ncloud_public_ip.this) != 0 ? ncloud_public_ip.this[0].public_ip : null
 }
