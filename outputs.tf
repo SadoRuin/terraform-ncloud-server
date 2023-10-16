a14d8ae00949da54fdabb8f6f9dd6cc0eafa0293
박윤환 <dope2514@gmail.com>
2023-10-17 01:46:40 +0900

    Add server_name output

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "server_id" {
   value       = ncloud_server.this.id
 }
 
+output "server_name" {
+  description = "서버 Name"
+  value       = ncloud_server.this.name
+}
+
 output "public_ip" {
   description = "서버 Public IP 주소"
   value       = length(ncloud_public_ip.this) != 0 ? ncloud_public_ip.this[0].public_ip : null
